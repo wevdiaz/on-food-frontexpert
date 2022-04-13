a69678d0be599f658b9fddedd0a14ebab8c99b08
Weverson Diazz <wevers.info@gmail.com>
2022-04-13 19:10:59 -0300

    configurando o path das exportação de maneira mais prática

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import { Header } from "../components";
+import { Header } from "@/components";
 
 const Home: NextPage = () => {
   return (
