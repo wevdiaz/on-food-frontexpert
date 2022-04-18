2ccb39cd9b53491a91bca129132a02ea0bea4760
Weverson Diazz <wevers.info@gmail.com>
2022-04-17 21:56:43 -0300

    Utilizando o componente de Menu para organizar as seleções

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -1,7 +1,22 @@
+import { Menu } from "@headlessui/react";
+
 export function Search() {
     return (
-        <div className="bg-gray-200 rounded-xl">
-            <p>Selecione uma cidade para iniciar</p>
-        </div>
+       <Menu as = "div">
+           <Menu.Button> Selecione uma cidade para iniciar </Menu.Button>
+           <Menu.Items>
+               <div>
+                   <Menu.Item>
+                       <span>São Paulo, SP</span>
+                   </Menu.Item>
+                   <Menu.Item>
+                       <span>Florianópolis, SC</span>
+                   </Menu.Item>
+                   <Menu.Item>
+                       <span>Porto Alegre, RS</span>
+                   </Menu.Item>
+               </div>
+           </Menu.Items>
+       </Menu>
     );
 }
