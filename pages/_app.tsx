f8d261b643fa3d4eae77afb8b2a953927a605b0b
Weverson Diazz <wevers.info@gmail.com>
2022-04-18 20:41:35 -0300

    instalando o axios e React-Query - configurando para a versão do React 18 o arquivo index.d.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from "react-query";
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp
