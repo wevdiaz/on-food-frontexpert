7864735f89a1a900aee36d340ae2c65635a20c6d
Weverson Diazz <wevers.info@gmail.com>
2022-04-18 22:23:55 -0300

    passando os dados da props e deixando a aplicação dinâmica

--- components/dish-list/DishList.tsx
+++ components/dish-list/DishList.tsx
@@ -1,15 +1,26 @@
 import Styles from "./DishList.module.css";
 
 import { Dish } from "@/components";
+import { api } from "@services";
+import { useQuery } from "react-query";
+import { DishType } from "core/types";
 
 export function DishList() {
+
+    const fetchDishes = async () => {
+        const response = await api.get<DishType[]>("/deliveries?city=sao-paulo-sp");
+        return response.data;
+    }
+
+    const { data, isFetching } = useQuery<DishType[]>("dishes", fetchDishes);
+
     return (
         <div className={Styles.list}>
-            <Dish />
-            <Dish />
-            <Dish />
-            <Dish />
-            <Dish />
+           {isFetching ? (
+               <div>Carregando...</div>
+           ) : (
+               data?.map((dish, index) => <Dish key={dish.id} { ...dish} /> )
+           )}
         </div>
     );
 }
--- components/dish/Dish.tsx
+++ components/dish/Dish.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import { DishDetails } from "../dish-details";
+import { DishType } from "core/types";
 import Styles from "./Dish.module.css";
 
-export function Dish() {
+export function Dish(props: DishType) {
     return (
         <div className={Styles.dish}>
             <div className={Styles.wrapper}>
-                <Image src="https://storage.googleapis.com/onfood/peperoni.jpg" alt="Prato" width={592} height={312} layout="responsive" />
+                <Image src={props.picture} alt={props.name} width={592} height={312} layout="responsive" />
             </div>
 
             <div>
                 <div className={Styles.header}>
-                    <h2>Nome do Produto</h2>
+                    <h2>{props.name}</h2>
                 </div>
                 <div className={Styles.details}>
-                    <DishDetails type="reviews" title="(201)" subtitle="4.5"/>
-                    <DishDetails type="category" title="Categoria" />
-                    <DishDetails type="delivery" title="30 - 40 min" />
+                    <DishDetails type="reviews" title={props.reviews} subtitle={props.rate}/>
+                    <DishDetails type="category" title={props.category} />
+                    <DishDetails type="delivery" title={props.deliveryEstimate} />
                 </div>
             </div>
 
